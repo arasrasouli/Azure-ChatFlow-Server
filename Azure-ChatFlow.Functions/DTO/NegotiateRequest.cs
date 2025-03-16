b8dff91f4427ce922df75590090b3b6f12b70b7b
Aras Rasouli <a.rasouli@gmail.com>
2025-03-17 00:58:51 +0100

    refactor: addDTOs to projects

---
+++ Azure-ChatFlow.Functions/DTO/NegotiateRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace AzureChatFlow.Functions.DTO
+{
+    public class NegotiateRequestDto
+    {
+        public string UserId { get; set; }
+    }
+
+    public class ConnectionRegistrationDto
+    {
+        public string UserId { get; set; }
+        public string ConnectionId { get; set; }
+    }
+}
