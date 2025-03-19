c27e1dd9dee79e4910fd872b6f92c979663f1bde
Aras Rasouli <a.rasouli@gmail.com>
2025-03-19 02:05:45 +0100

    refactor: define models in common layer

---
+++ Azure-ChatFlow.Common/Model/ConnectionRegistrationModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AzureChatFlow.Common.Model
+{
+    public class ConnectionRegistrationModel
+    {
+        public string UserId { get; set; }
+        public string ConnectionId { get; set; }
+    }
+}
---
+++ Azure-ChatFlow.Common/Model/MessageModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureChatFlow.Common.Model
+{
+    public class MessageModel
+    {
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public string Message { get; set; }
+        public DateTime SendAt { get; set; }
+    }
+}
