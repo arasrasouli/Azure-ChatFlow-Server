f46696018eb141225080968ea0fb8685ebe64fdd
Aras Rasouli <a.rasouli@gmail.com>
2025-03-19 02:04:14 +0100

    refactor: define and implement signalr client interface and class in infrastructure layer

---
+++ Azure-ChatFlow.Infrastructure/SignalR/ISignalRClient.cs
@@ -0,0 +1,10 @@
+﻿using AzureChatFlow.Common.Model;
+
+namespace AzureChatFlow.Infrastructure.SignalR
+{
+    public interface ISignalRClient
+    {
+        Task SendToClientAsync(string connectionId, string methodName, MessageModel messageData);
+        Task<ConnectionInfoModel> NegotiateConnectionAsync(string userId);
+    }
+}
