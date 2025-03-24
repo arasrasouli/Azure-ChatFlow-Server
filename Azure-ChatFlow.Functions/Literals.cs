d3bde300fbc835a343c0a0eaf3a71841b21b6d9d
Aras Rasouli <a.rasouli@gmail.com>
2025-03-25 00:29:27 +0100

    chore: update configurations, dependencies and references

---
+++ Azure-ChatFlow.Functions/Literals.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureChatFlow.Functions
+{
+    public static class Literals
+    {
+        public const string AzureWebJobsStorage = nameof(AzureWebJobsStorage);
+        public const string TableStorageConnectionString = nameof(TableStorageConnectionString);
+        public const string ChatTableName = nameof(ChatTableName);
+        public const string AzureSignalRConnectionString = nameof(AzureSignalRConnectionString);
+    }
+}
