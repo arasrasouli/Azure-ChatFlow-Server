c3de98fdb808dc0f51d2d599bc0c5dcaae2ff3c3
Aras Rasouli <a.rasouli@gmail.com>
2025-03-25 00:25:26 +0100

    feat: add needed models, helper and data model

---
+++ Azure-ChatFlow.Common/Enum/ChatEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace AzureChatFlow.Common.Enum
+{
+    public class ChatEnum
+    {
+        public enum ReadStatus : int
+        {
+            NotSent = 0,
+            Sent = 1,
+            Seen = 2
+        }
+    }
+}
